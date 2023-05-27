813e3feaa4cc7c3e2fb3e743fc0566182acfbb42
cybzzz <cybzzz@foxmail.com>
2023-05-27 17:45:38 +0800

    update gitalk config

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -24,7 +24,7 @@ onContentUpdated(() => {
     repo: "blog-comments",
     owner: "cybzzz",
     admin: ["cybzzz"],
-    id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
+    id: location.pathname, // Ensure uniqueness and length less than 50
     language: "zh-CN",
     distractionFreeMode: true, // Facebook-like distraction free mode
   });
